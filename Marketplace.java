6ed623c23d31ed7968be99664f85b98ac11c9187
liammmack <129445378+liammmack@users.noreply.github.com>
2023-04-02 18:29:54 -0400

    Update Marketplace.java

--- Marketplace.java
+++ Marketplace.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
 public class Marketplace {
 
-    public static String welcome = "Welcome to the Marketplace!";
-    public static String welcomeMenu = "Please select an option:\n(1) Login\n(2) Create an Account\n(3) Quit";
+    public static String WELCOME = "Welcome to the Marketplace!";
+    public static String WELCOMEMENU = "Please select an option:\n(1) Login\n(2) Create an Account\n(3) Quit";
+    public static String LINES = "-----------------";
+    public static String USERNAME = "UserName:";
+    public static String PASSWORD = "Password:";
+    public static String INCORRECTCREDENTIALS = "Incorrect login credentials or account does not exist, please try again.";
+    public static String LOGINSUCCESS = "Login Successful";
+    public static String USEREXISTS = "Username is taken, please try again";
+    public static String ACCOUNTCREATED = "Account successfully created!";
     public static void main(String[] args) {
         int welcomeOption;
 
         Scanner sc = new Scanner(System.in);
-        System.out.println(welcome);
+        System.out.println(WELCOME);
 
         do {
-            System.out.println(welcomeMenu);
-            welcomeOption = sc.nextInt();
+            System.out.println(WELCOMEMENU);
+            welcomeOption = Integer.parseInt(sc.nextLine());
 
             if (welcomeOption == 1) {
                 System.out.println("Login Menu");
@@ -19,11 +26,50 @@ public class Marketplace {
                 System.out.println("Create New Account");
             } else if (welcomeOption == 3) {
                 System.out.println("Bye!");
+                return;
             } else {
                 System.out.println("Invalid Option!");
             }
 
         } while (welcomeOption > 3 || welcomeOption < 1);
 
+        if (welcomeOption == 1) {
+            boolean loginBoolean = true;
+            while(loginBoolean) {
+                System.out.println(LINES);
+                System.out.println(USERNAME);
+                String username = sc.nextLine();
+                System.out.println(PASSWORD);
+                String password = sc.nextLine();
+
+                if (userExits(username) == false)
+                {
+                    System.out.println(INCORRECTCREDENTIALS);
+                }
+                else if (userExists(username)) {
+                    if(isCorrectLogin(username, password)) {
+                        System.out.println(LOGINSUCCESS);
+                        loginBoolean = false;
+                    } else {
+                        System.out.println(INCORRECTCREDENTIALS);
+                    }
+                }
+            }
+        }
+
+        else if (welcomeOption == 2) {
+            System.out.println(LINES);
+            System.out.println("Create Account");
+            System.out.println(USERNAME);
+            String username = sc.nextLine();
+            System.out.println(PASSWORD);
+            String password = sc.nextLine();
+
+            if(userExists(username)) {
+                System.out.println(USEREXISTS);
+            } else {
+                System.out.println(ACCOUNTCREATED);
+            }
+        }
     }
 }
