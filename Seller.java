cc072ce96ff6f4ce2033a3f4bdbd76bb07492597
Tristen Brideweser <113396476+tbridewe@users.noreply.github.com>
2023-04-08 17:18:38 -0400

    Update Seller.java

--- Seller.java
+++ Seller.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Seller extends User {
     private ArrayList<String> stores;
@@ -8,6 +10,14 @@ public class Seller extends User {
     public Seller (String email, String password, int userType) throws InvalidUserInput {
         super(email, password, userType);
         this.stores = new ArrayList<>();
+    }
+
+    public ArrayList<String> getStores() {
+        return stores;
+    }
+
+    public void setStores(ArrayList<String> stores) {
+        this.stores = stores;
     }
 
     public static String[] readFile(String filename) {
@@ -68,6 +78,7 @@ public class Seller extends User {
         }
     }
 
+    // DONE
     public static void removeItem(Item item) {
         ArrayList<String> sellerItems = displayItems(); // seller items
 
@@ -80,9 +91,10 @@ public class Seller extends User {
         }
     }
 
+    // DONE
     public void editItem(Item item, String changeType, String changeValue ) {
         ArrayList<String> sellerItems = displayItems();
-        
+
         if (sellerItems.contains(String.valueOf(item))) {
             for (int i = 0; i < sellerItems.size(); i++) { // loop through the seller items
                 if (sellerItems.get(i).equals(String.valueOf(item))) { // find the item
@@ -104,8 +116,12 @@ public class Seller extends User {
             System.out.println("Item does not exist!");
         }
     }
-    public void viewSpecificStat() {
 
+    public void viewSpecificStat(String statType) {
+        switch (statType) {
+            case "Price":
+            case "Quantity":
+        }
     }
 
     public static void viewAllStats() {
@@ -117,6 +133,79 @@ public class Seller extends User {
     }
 
     public void sortStats(String sortType, String sortOrder) {
-        if ()
+        String[] stats = readFile("CustomerLog.txt");
+        ArrayList<String> statsList = new ArrayList<>();
+        ArrayList<String> sortedStatsList = new ArrayList<>();
+        
+        Collections.addAll(statsList, stats);
+
+        switch (sortType) {
+            case "Price":
+                switch (sortOrder) {
+                    case "Ascending":
+                        statsList.sort(new PriceComparatorAscending());
+                        for (int i = 0; i < statsList.size(); i++) {
+                            System.out.println(statsList.get(i));
+                        }
+                    case "Descending":
+                        statsList.sort(new PriceComparatorDescending());
+                        for (int i = 0; i < statsList.size(); i++) {
+                            System.out.println(statsList.get(i));
+                        }
+                }
+            case "Quantity":
+                switch (sortOrder) {
+                    case "Ascending":
+                        statsList.sort(new QuantityComparatorAscending());
+                        for (int i = 0; i < statsList.size(); i++) {
+                            System.out.println(statsList.get(i));
+                        }
+                    case "Descending":
+                        statsList.sort(new QuantityComparatorDescending());
+                        for (int i = 0; i < statsList.size(); i++) {
+                            System.out.println(statsList.get(i));
+                        }
+                }
+        }
+    }
+
+    // Comparator for sorting by price in ascending order
+    static class PriceComparatorAscending implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            double price1 = Double.parseDouble(o1.split(",")[4]);
+            double price2 = Double.parseDouble(o2.split(",")[4]);
+            return Double.compare(price1, price2);
+        }
+    }
+
+    // Comparator for sorting by price in descending order
+    static class PriceComparatorDescending implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            double price1 = Double.parseDouble(o1.split(",")[4]);
+            double price2 = Double.parseDouble(o2.split(",")[4]);
+            return Double.compare(price2, price1);
+        }
+    }
+
+    // Comparator for sorting by price in ascending order
+    static class QuantityComparatorAscending implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            int quantity1 = Integer.parseInt(o1.split(",")[3]);
+            int quantity2 = Integer.parseInt(o2.split(",")[3]);
+            return Double.compare(quantity1, quantity2);
+        }
+    }
+
+    // Comparator for sorting by price in descending order
+    static class QuantityComparatorDescending implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            int quantity1 = Integer.parseInt(o1.split(",")[3]);
+            int quantity2 = Integer.parseInt(o2.split(",")[3]);
+            return Double.compare(quantity2, quantity1);
+        }
     }
 }
