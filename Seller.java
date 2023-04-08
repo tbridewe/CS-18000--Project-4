a58d2cc95cd933694399dc8a792b966e79af05a3
Tristen Brideweser <113396476+tbridewe@users.noreply.github.com>
2023-04-08 16:35:08 -0400

    Update Seller.java

--- Seller.java
+++ Seller.java
@@ -28,11 +28,11 @@ public class Seller extends User {
         fileContents = contents.toArray(new String[0]);
         return fileContents;
     }
-    
+
     public static writeFile(String filename) {
-        
+
     }
-    
+
     public static ArrayList<String> displayItems() {
         String[] contents = readFile("listings.txt");
         ArrayList<String> sellerItems = new ArrayList<>();
@@ -45,33 +45,64 @@ public class Seller extends User {
         }
         return sellerItems;
     }
-    
+
     public void addFromCSV(String filename) {
         String[] csvData = readFile(filename);
 
         for (int i = 0; i < csvData.length; i++) {
-            // write to itemListings.txt 
+            // write to itemListings.txt
         }
 
     }
 
     public static void addNewItem(Item item) {
         ArrayList<String> sellerItems = displayItems();
+
+        for (int i = 0; i < sellerItems.size(); i++) { // loop through all the seller items
+            if (sellerItems.get(i).contains()))
+        }
         if (sellerItems.contains(String.valueOf(item))) { // if item already exists in item listings, just update quantity
-            item.getQuantity()
+            item.setQuantity(item.getQuantity() + );
         } else {
             sellerItems.set(sellerItems.size() + 1, String.valueOf(item)); // add new entry into sellerItems
         }
     }
 
     public static void removeItem(Item item) {
-        ArrayList<String> sellerItems = displayItems();
-        sellerItems.remove()
+        ArrayList<String> sellerItems = displayItems(); // seller items
+
+        for (int i = 0; i < sellerItems.size(); i++) { // loop through seller items
+            if (sellerItems.contains(String.valueOf(item))) { // if the item is in seller items, remove it
+                sellerItems.remove(String.valueOf(item));
+            } else {
+                System.out.println("Item does not exist!"); // display error message if not in the seller items
+            }
+        }
     }
 
-    public void editItem() {
-        displayItems();
-
+    public void editItem(Item item, String changeType, String changeValue ) {
+        ArrayList<String> sellerItems = displayItems();
+        
+        if (sellerItems.contains(String.valueOf(item))) {
+            for (int i = 0; i < sellerItems.size(); i++) { // loop through the seller items
+                if (sellerItems.get(i).equals(String.valueOf(item))) { // find the item
+                    switch (changeType) { // choose what changeType
+                        case "Name":
+                            item.setName(changeValue);
+                        case "Store":
+                            item.setStore(changeValue);
+                        case "Description":
+                            item.setDescription(changeValue);
+                        case "Quantity":
+                            item.setQuantity(Integer.parseInt(changeValue));
+                        case "Price":
+                            item.setPrice(Integer.parseInt(changeValue));
+                    }
+                }
+            }
+        } else {
+            System.out.println("Item does not exist!");
+        }
     }
     public void viewSpecificStat() {
 
@@ -89,56 +120,3 @@ public class Seller extends User {
         if ()
     }
 }
-    /*
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("1. View store statistics\n2. View listings");
-        int view = scanner.nextInt();
-        scanner.nextLine();
-        if (view == 1) {
-            if (User.userExists && User.isCorrectLogin && User.hasStore) { //if seller exists and has a store
-                System.out.println("1. View all statistics\n2. View specific statistics");
-                int whichStatistics = scanner.nextInt();
-                scanner.nextLine();
-                if (whichStatistics == 1) {
-                    System.out.println(this.store);  //view all of the store statistics
-                } else if (whichStatistics == 2) {
-                    System.out.println(); //which statistics do you want to view
-                    //view the store statistics specified by the value
-                } else {
-                    throw new InvalidUserInput(e) {
-                        e.printStackTrace;
-                    }
-                }
-            } else {
-                throw new IncorrectLoginInformation(e) {
-                    e.printStackTrace;
-                }
-            }
-        } else if (view == 2) {
-            //view all store listings
-            System.out.println("1. Add\n2. Edit\n 3. Delete");
-            int storeListingFunctions = scanner.nextInt();
-            scanner.nextLine();
-            if (storeListingFunctions == 1) { //add
-                //input csv values
-            } else if (storeListingFunctions == 2) { //edit
-                //input which listing
-                //input which value
-                //change value
-            } else if (storeListingFunctions == 3) { //delete
-                //remove listing/move other things up
-            }else {
-                throw new InvalidUserInput(e) {
-                    e.printStackTrace;
-                }
-            }
-        } else {
-            throw new InvalidUserInput(e) {
-                e.printStackTrace;
-            }
-        }
-    }
-}
-
-     */
